f84d61910ee9bb933d6775093fafea3527413850
eflerrr <cool.yan_sv@mail.ru>
2024-04-08 17:44:25 +0300

    DES done on top of the Feistel Network

---
+++ des/src/main/java/org/eflerrr/encrypt/encryptor/impl/DESEncryptor.java
@@ -0,0 +1,30 @@
+package org.eflerrr.encrypt.encryptor.impl;
+
+import org.eflerrr.encrypt.conversion.impl.DESEncryptConversion;
+import org.eflerrr.expandkey.impl.DESExpandKey;
+
+import static org.eflerrr.config.des.DESEncryptorConfig.FINAL_PERMUTATION;
+import static org.eflerrr.config.des.DESEncryptorConfig.INITIAL_PERMUTATION;
+import static org.eflerrr.utils.Utils.permutation;
+
+public class DESEncryptor extends FeistelNetworkEncryptor {
+
+    public DESEncryptor() {
+        super(new DESEncryptConversion(), new DESExpandKey(), 16);
+    }
+
+    @Override
+    public byte[] encrypt(byte[] block) {
+        var initialPermutedBlock = permutation(block, INITIAL_PERMUTATION);
+        var encryptedBlock = super.encrypt(initialPermutedBlock);
+        return permutation(encryptedBlock, FINAL_PERMUTATION);
+    }
+
+    @Override
+    public byte[] decrypt(byte[] block) {
+        var initialPermutedBlock = permutation(block, INITIAL_PERMUTATION);
+        var encryptedBlock = super.decrypt(initialPermutedBlock);
+        return permutation(encryptedBlock, FINAL_PERMUTATION);
+    }
+
+}
