6932988ebe322e4d714d4f7a2e8064a261cf056a
eflerrr <cool.yan_sv@mail.ru>
2024-05-01 04:33:11 +0300

    adding new and refactoring old tests!

--- des/src/main/java/org/eflerrr/Main.java
+++ des/src/main/java/org/eflerrr/Main.java
@@ -1,16 +1,16 @@
 package org.eflerrr;
 
 import lombok.extern.slf4j.Slf4j;
-import org.eflerrr.encrypt.encryptor.impl.DESEncryptor;
 import org.eflerrr.encrypt.manager.EncryptorManager;
 import org.eflerrr.encrypt.mode.EncryptModes;
 import org.eflerrr.padding.Paddings;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
-import static org.eflerrr.encrypt.mode.EncryptModes.Mode.CTR;
+import static org.eflerrr.encrypt.manager.EncryptorManager.EncryptionAlgorithm.DEAL_256;
+import static org.eflerrr.encrypt.manager.EncryptorManager.EncryptionAlgorithm.DES;
+import static org.eflerrr.encrypt.mode.EncryptModes.Mode.RandomDelta;
 import static org.eflerrr.padding.Paddings.PaddingType.ISO10126;
 import static org.eflerrr.utils.Utils.generateIV;
 
@@ -18,21 +18,53 @@ import static org.eflerrr.utils.Utils.generateIV;
 @SuppressWarnings("Duplicates")
 public class Main {
 
-    public static void main(String[] args) throws URISyntaxException, IOException {
+    public static void main(String[] args) {
 
         //----------------------------
 
-        // configs ##############################
-        var key = new byte[]{                                           // key = 133457788BBCDFF1
+        // configs ##############################|
+        // key = 133457788BBCDFF1
+        var keyForDES = new byte[]{
                 (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
                 (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1
         };
+        // key = 133457788BBCDFF11255F9E7911355BD
+        var keyForDEAL128 = new byte[]{
+                (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
+                (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1,
+                (byte) 0x12, (byte) 0x55, (byte) 0xF9, (byte) 0xE7,
+                (byte) 0x91, (byte) 0x13, (byte) 0x55, (byte) 0xBD
+        };
+        // key = 133457788BBCDFF11255F9E7911355BD802F17DAAAC805CC
+        var keyForDEAL192 = new byte[]{
+                (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
+                (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1,
+                (byte) 0x12, (byte) 0x55, (byte) 0xF9, (byte) 0xE7,
+                (byte) 0x91, (byte) 0x13, (byte) 0x55, (byte) 0xBD,
+                (byte) 0x80, (byte) 0x2F, (byte) 0x17, (byte) 0xDA,
+                (byte) 0xAA, (byte) 0xC8, (byte) 0x05, (byte) 0xCC
+        };
+        // key = 133457788BBCDFF11255F9E7911355BD802F17DAAAC805CCFFEDCBA72233DDC0
+        var keyForDEAL256 = new byte[]{
+                (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
+                (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1,
+                (byte) 0x12, (byte) 0x55, (byte) 0xF9, (byte) 0xE7,
+                (byte) 0x91, (byte) 0x13, (byte) 0x55, (byte) 0xBD,
+                (byte) 0x80, (byte) 0x2F, (byte) 0x17, (byte) 0xDA,
+                (byte) 0xAA, (byte) 0xC8, (byte) 0x05, (byte) 0xCC,
+                (byte) 0xFF, (byte) 0xED, (byte) 0xCB, (byte) 0xA7,
+                (byte) 0x22, (byte) 0x33, (byte) 0xDD, (byte) 0xC0
+        };
 
-        EncryptModes.Mode mode = CTR;
+        EncryptorManager.EncryptionAlgorithm algorithm = DEAL_256;
+        EncryptModes.Mode mode = RandomDelta;
         Paddings.PaddingType type = ISO10126;
-        int threads = 5;
+        int threads = 7;
 
-        var inputFile = "message.txt";
+        var inputFile = "deadinside.jpg";
+        var folder = "image";
+        // configs ##############################|
+
         var encryptedFile = inputFile.substring(0, inputFile.lastIndexOf('.'))
                 + "_encrypted"
                 + inputFile.substring(inputFile.lastIndexOf('.'));
@@ -40,15 +72,18 @@ public class Main {
                 + "_decrypted"
                 + inputFile.substring(inputFile.lastIndexOf('.'));
 
-        var folder = "image";
-        // configs ##############################
-
         var manager = new EncryptorManager(
-                key, new DESEncryptor(),
-                mode, type, generateIV(64 / 8)
+                switch (algorithm) {
+                    case DES -> keyForDES;
+                    case DEAL_128 -> keyForDEAL128;
+                    case DEAL_192 -> keyForDEAL192;
+                    case DEAL_256 -> keyForDEAL256;
+                },
+                algorithm, mode, type, generateIV((algorithm.equals(DES) ? 64 : 128) / 8)
         );
 
         try {
+            log.info("Algorithm: {}", algorithm);
             log.info("Mode: {}", mode);
             log.info("Type: {}", type);
             log.info("Threads: {}", threads);
@@ -84,10 +119,17 @@ public class Main {
         //----------------------------
 
         /*
-        // key = 133457788BBCDFF1
-        byte[] key = new byte[]{
+        // keyForDES = 133457788BBCDFF1
+        byte[] keyForDES = new byte[]{
                 (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
                 (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1
+        };
+        // keyForDEAL = 133457788BBCDFF11255F9E7911355BD
+        var keyForDEAL = new byte[]{
+                (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
+                (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1,
+                (byte) 0x12, (byte) 0x55, (byte) 0xF9, (byte) 0xE7,
+                (byte) 0x91, (byte) 0x13, (byte) 0x55, (byte) 0xBD
         };
         log.debug("[Key] hex: {}, binary: {}",
                 bytesToHexString(key), bytesToBinaryString(key, "-"))
@@ -141,21 +183,29 @@ public class Main {
                 :)
                 """;
 
-        byte[] key = new byte[]{
+        // keyForDES = 133457788BBCDFF1
+        byte[] keyForDES = new byte[]{
                 (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
                 (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1
+        };
+        // keyForDEAL = 133457788BBCDFF11255F9E7911355BD
+        var keyForDEAL = new byte[]{
+                (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
+                (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1,
+                (byte) 0x12, (byte) 0x55, (byte) 0xF9, (byte) 0xE7,
+                (byte) 0x91, (byte) 0x13, (byte) 0x55, (byte) 0xBD
         };
 
         byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
 
         var manager = new EncryptorManager(
-                key, new DESEncryptor(), CTR, ISO10126, generateIV(64 / 8)
+                keyForDEAL, DEAL_128, ECB, ZEROZ, new byte[128 / 8]
         );
 
         var encryptedMessage = manager.encryptAsync(messageBytes, 5);
         log.info("##### Encrypted message #####\n" + new String(encryptedMessage, StandardCharsets.UTF_8));
 
-        log.info();
+        log.info("");
 
         var decryptedMessage = manager.decryptAsync(encryptedMessage, 5);
         log.info("##### Decrypted message #####\n" + new String(decryptedMessage, StandardCharsets.UTF_8));
--- des/src/test/java/EncryptorManagerTest.java
+++ des/src/test/java/EncryptorManagerTest.java
@@ -1,5 +1,4 @@
 import lombok.extern.slf4j.Slf4j;
-import org.eflerrr.encrypt.encryptor.impl.DESEncryptor;
 import org.eflerrr.encrypt.manager.EncryptorManager;
 import org.eflerrr.encrypt.mode.EncryptModes;
 import org.eflerrr.padding.Paddings;
@@ -17,6 +16,7 @@ import java.nio.file.Paths;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import static org.eflerrr.encrypt.manager.EncryptorManager.EncryptionAlgorithm.DES;
 import static org.eflerrr.utils.Utils.bytesToHexString;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -48,11 +48,44 @@ public class EncryptorManagerTest {
         log.trace("");
     }
 
+
+    // -------- keys -------- //
+
     // key = 133457788BBCDFF1
-    public static final byte[] KEY = new byte[]{
+    public static final byte[] KEY_FOR_DES = new byte[]{
             (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
             (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1
     };
+    // key = 133457788BBCDFF11255F9E7911355BD
+    public static final byte[] KEY_FOR_DEAL_128 = new byte[]{
+            (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
+            (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1,
+            (byte) 0x12, (byte) 0x55, (byte) 0xF9, (byte) 0xE7,
+            (byte) 0x91, (byte) 0x13, (byte) 0x55, (byte) 0xBD
+    };
+    // key = 133457788BBCDFF11255F9E7911355BD802F17DAAAC805CC
+    public static final byte[] KEY_FOR_DEAL_192 = new byte[]{
+            (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
+            (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1,
+            (byte) 0x12, (byte) 0x55, (byte) 0xF9, (byte) 0xE7,
+            (byte) 0x91, (byte) 0x13, (byte) 0x55, (byte) 0xBD,
+            (byte) 0x80, (byte) 0x2F, (byte) 0x17, (byte) 0xDA,
+            (byte) 0xAA, (byte) 0xC8, (byte) 0x05, (byte) 0xCC
+    };
+    // key = 133457788BBCDFF11255F9E7911355BD802F17DAAAC805CCFFEDCBA72233DDC0
+    public static final byte[] KEY_FOR_DEAL_256 = new byte[]{
+            (byte) 0x13, (byte) 0x34, (byte) 0x57, (byte) 0x79,
+            (byte) 0x9B, (byte) 0xBC, (byte) 0xDF, (byte) 0xF1,
+            (byte) 0x12, (byte) 0x55, (byte) 0xF9, (byte) 0xE7,
+            (byte) 0x91, (byte) 0x13, (byte) 0x55, (byte) 0xBD,
+            (byte) 0x80, (byte) 0x2F, (byte) 0x17, (byte) 0xDA,
+            (byte) 0xAA, (byte) 0xC8, (byte) 0x05, (byte) 0xCC,
+            (byte) 0xFF, (byte) 0xED, (byte) 0xCB, (byte) 0xA7,
+            (byte) 0x22, (byte) 0x33, (byte) 0xDD, (byte) 0xC0
+    };
+
+
+    // -------- plain texts -------- //
 
     // plain text (exact length) = 0123456789ABCDEF
     public static final byte[] PLAIN_TEXT_EXACT_LENGTH = new byte[]{
@@ -66,32 +99,64 @@ public class EncryptorManagerTest {
             (byte) 0xAE, (byte) 0x2D
     };
 
+
+    // -------- Initialization vectors -------- //
+
     // initialization vector = 1F3400789AB5DEF7
-    private static final byte[] IV = new byte[]{
+    private static final byte[] IV_DES = new byte[]{
             (byte) 0x1F, (byte) 0x34, (byte) 0x00, (byte) 0x78,
             (byte) 0x9A, (byte) 0xB5, (byte) 0xDE, (byte) 0xF7
     };
+    // initialization vector = 1F3400789AB5DEF777350D7AAABBCCDD
+    private static final byte[] IV_DEAL = new byte[]{
+            (byte) 0x1F, (byte) 0x34, (byte) 0x00, (byte) 0x78,
+            (byte) 0x9A, (byte) 0xB5, (byte) 0xDE, (byte) 0xF7,
+            (byte) 0x77, (byte) 0x35, (byte) 0x0D, (byte) 0x7A,
+            (byte) 0xAA, (byte) 0xBB, (byte) 0xCC, (byte) 0xDD
+    };
 
-    // ------------------------------
+
+    // -------- Data providers -------- //
 
     private static Stream<Arguments> provideTestData() {
-        return Stream.of(EncryptModes.Mode.values())
-                .flatMap(mode -> Stream.of(Paddings.PaddingType.values())
-                        .map(type -> Arguments.of(mode, type)));
+        return Stream.of(EncryptorManager.EncryptionAlgorithm.values())
+                .flatMap(algorithm -> Stream.of(EncryptModes.Mode.values())
+                        .flatMap(mode -> Stream.of(Paddings.PaddingType.values())
+                                .map(type -> Arguments.of(
+                                        algorithm, mode, type))));
     }
 
     private static Stream<Arguments> provideFileTestData() {
-        return Stream.of(EncryptModes.Mode.values())
-                .flatMap(mode -> Stream.of(Paddings.PaddingType.values())
-                        .flatMap(type -> IntStream.range(1, 9)
-                                .mapToObj(threads -> Arguments.of(mode, type, threads))));
+        return Stream.of(EncryptorManager.EncryptionAlgorithm.values())
+                .flatMap(algorithm -> Stream.of(EncryptModes.Mode.values())
+                        .flatMap(mode -> Stream.of(Paddings.PaddingType.values())
+                                .flatMap(type -> IntStream.range(1, 9)
+                                        .mapToObj(threads -> Arguments.of(
+                                                algorithm, mode, type, threads)))));
     }
+
+
+    // -------- Blocks enctyption tests -------- //
 
     @ParameterizedTest
     @MethodSource("provideTestData")
-    public void encryptSyncBlockExactLengthTest(EncryptModes.Mode mode, Paddings.PaddingType type) {
-        EncryptorManager manager = new EncryptorManager(KEY, new DESEncryptor(), mode, type, IV);
+    public void encryptSyncBlockExactLengthTest(
+            EncryptorManager.EncryptionAlgorithm algorithm,
+            EncryptModes.Mode mode,
+            Paddings.PaddingType type
+    ) {
+        var KEY = switch (algorithm) {
+            case DES -> KEY_FOR_DES;
+            case DEAL_128 -> KEY_FOR_DEAL_128;
+            case DEAL_192 -> KEY_FOR_DEAL_192;
+            case DEAL_256 -> KEY_FOR_DEAL_256;
+        };
+        var IV = algorithm.equals(DES)
+                ? IV_DES : IV_DEAL;
+        EncryptorManager manager = new EncryptorManager(KEY, algorithm, mode, type, IV);
+
         log.info("--- ENCRYPTING PLAIN BYTES (EXACT LENGTH) ---");
+        log.info("Algorithm: {}", algorithm);
         log.info("Mode: {}", mode);
         log.info("Padding: {}", type);
         log.info("Plain text:  hex -> {} ", bytesToHexString(PLAIN_TEXT_EXACT_LENGTH));
@@ -99,14 +164,29 @@ public class EncryptorManagerTest {
         log.info("Encrypted:  hex -> {} ", bytesToHexString(encrypted));
         byte[] decrypted = manager.decryptSync(encrypted);
         log.info("Decrypted:  hex -> {} ", bytesToHexString(decrypted));
+
         assertArrayEquals(PLAIN_TEXT_EXACT_LENGTH, decrypted);
     }
 
     @ParameterizedTest
     @MethodSource("provideTestData")
-    public void encryptSyncBlockWrongLengthTest(EncryptModes.Mode mode, Paddings.PaddingType type) {
-        EncryptorManager manager = new EncryptorManager(KEY, new DESEncryptor(), mode, type, IV);
+    public void encryptSyncBlockWrongLengthTest(
+            EncryptorManager.EncryptionAlgorithm algorithm,
+            EncryptModes.Mode mode,
+            Paddings.PaddingType type
+    ) {
+        var KEY = switch (algorithm) {
+            case DES -> KEY_FOR_DES;
+            case DEAL_128 -> KEY_FOR_DEAL_128;
+            case DEAL_192 -> KEY_FOR_DEAL_192;
+            case DEAL_256 -> KEY_FOR_DEAL_256;
+        };
+        var IV = algorithm.equals(DES)
+                ? IV_DES : IV_DEAL;
+        EncryptorManager manager = new EncryptorManager(KEY, algorithm, mode, type, IV);
+
         log.info("--- ENCRYPTING PLAIN BYTES (WRONG LENGTH) ---");
+        log.info("Algorithm: {}", algorithm);
         log.info("Mode: {}", mode);
         log.info("Padding: {}", type);
         log.info("Plain text:  hex -> {} ", bytesToHexString(PLAIN_TEXT_WRONG_LENGTH));
@@ -114,16 +194,33 @@ public class EncryptorManagerTest {
         log.info("Encrypted:  hex -> {} ", bytesToHexString(encrypted));
         byte[] decrypted = manager.decryptSync(encrypted);
         log.info("Decrypted:  hex -> {} ", bytesToHexString(decrypted));
+
         assertArrayEquals(PLAIN_TEXT_WRONG_LENGTH, decrypted);
     }
+
+
+    // -------- File async enctyption tests -------- //
 
     @ParameterizedTest
     @MethodSource("provideFileTestData")
     public void encryptAsyncTextMessageFromFileTest(
-            EncryptModes.Mode mode, Paddings.PaddingType type, int threads
+            EncryptorManager.EncryptionAlgorithm algorithm,
+            EncryptModes.Mode mode,
+            Paddings.PaddingType type,
+            int threads
     ) throws IOException {
-        EncryptorManager manager = new EncryptorManager(KEY, new DESEncryptor(), mode, type, IV);
+        var KEY = switch (algorithm) {
+            case DES -> KEY_FOR_DES;
+            case DEAL_128 -> KEY_FOR_DEAL_128;
+            case DEAL_192 -> KEY_FOR_DEAL_192;
+            case DEAL_256 -> KEY_FOR_DEAL_256;
+        };
+        var IV = algorithm.equals(DES)
+                ? IV_DES : IV_DEAL;
+        EncryptorManager manager = new EncryptorManager(KEY, algorithm, mode, type, IV);
+
         log.info("--- ENCRYPTING TEXT MESSAGE FROM FILE ---");
+        log.info("Algorithm: {}", algorithm);
         log.info("Mode: {}", mode);
         log.info("Padding: {}", type);
         log.info("Threads: {}", threads);
