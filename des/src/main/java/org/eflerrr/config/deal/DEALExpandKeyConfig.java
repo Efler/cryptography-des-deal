e278311208baaddd8b6e96045d2d1030276b3485
eflerrr <cool.yan_sv@mail.ru>
2024-05-01 04:19:47 +0300

    configuring fixed key for DEAL key-expanding

---
+++ des/src/main/java/org/eflerrr/config/deal/DEALExpandKeyConfig.java
@@ -0,0 +1,10 @@
+package org.eflerrr.config.deal;
+
+public final class DEALExpandKeyConfig {
+
+    public static final byte[] FIXED_DES_KEY = {
+            (byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78,
+            (byte) 0x90, (byte) 0xAB, (byte) 0xCD, (byte) 0xEF
+    };
+
+}
