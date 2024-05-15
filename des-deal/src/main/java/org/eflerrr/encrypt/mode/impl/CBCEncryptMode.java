e996c5ecf25dcc838aae26354977fba3d37eb2b8
eflerrr <cool.yan_sv@mail.ru>
2024-05-15 16:07:57 +0300

    refactoring CBC decryption for async process

--- des-deal/src/main/java/org/eflerrr/encrypt/mode/impl/CBCEncryptMode.java
+++ des-deal/src/main/java/org/eflerrr/encrypt/mode/impl/CBCEncryptMode.java
@@ -3,6 +3,12 @@ package org.eflerrr.encrypt.mode.impl;
 import org.eflerrr.encrypt.encryptor.IEncryptor;
 import org.eflerrr.encrypt.mode.AEncryptMode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static org.eflerrr.utils.Utils.xorBits;
 
 @SuppressWarnings("Duplicates")
@@ -27,21 +33,51 @@ public class CBCEncryptMode extends AEncryptMode {
         return result;
     }
 
+    private void decryptProcess(byte[] input, byte[] output, int i) {
+        byte[] prev = new byte[lengthBlock];
+        if (i == 0) {
+            prev = IV;
+        } else {
+            System.arraycopy(
+                    input, (i - 1) * lengthBlock,
+                    prev, 0,
+                    lengthBlock
+            );
+        }
+        int offset = i * lengthBlock;
+        byte[] block = new byte[lengthBlock];
+        System.arraycopy(input, offset, block, 0, lengthBlock);
+        byte[] decryptedBlock = xorBits(prev, encryptor.decrypt(block));
+        System.arraycopy(decryptedBlock, 0, output, offset, decryptedBlock.length);
+    }
+
     @Override
     public byte[] decrypt(byte[] data) {
-        byte[] result = new byte[data.length];
-        byte[] prev = IV;
-        for (int i = 0; i < data.length / lengthBlock; ++i) {
-            int offset = i * lengthBlock;
-            byte[] block = new byte[lengthBlock];
-            byte[] tmp = new byte[lengthBlock];
-            System.arraycopy(data, offset, block, 0, lengthBlock);
-            System.arraycopy(data, offset, tmp, 0, lengthBlock);
-            byte[] decryptedBlock = xorBits(prev, encryptor.decrypt(block));
-            System.arraycopy(decryptedBlock, 0, result, offset, decryptedBlock.length);
-            prev = tmp;
+        try (var executor = Executors.newFixedThreadPool(
+                Runtime.getRuntime().availableProcessors() - 1)) {
+            byte[] result = new byte[data.length];
+            List<CompletableFuture<Void>> futures = new ArrayList<>();
+            for (int i = 0; i < data.length / lengthBlock; i++) {
+                int finalI = i;
+                futures.add(
+                        CompletableFuture.runAsync(
+                                () -> decryptProcess(data, result, finalI), executor
+                        ));
+            }
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+            executor.shutdown();
+            try {
+                if (!executor.awaitTermination(2, TimeUnit.SECONDS)) {
+                    executor.shutdownNow();
+                }
+                return result;
+            } catch (InterruptedException e) {
+                executor.shutdownNow();
+                Thread.currentThread().interrupt();
+                return result;
+            }
         }
-        return result;
     }
 
 }
